e85df4a4cae539bff0e0edb3daf8e6b821e41c4d
yureistar <dng1@stuy.edu>
2016-12-08 11:32:18 -0500

    update

--- rsctrl.c
+++ rsctrl.c
@@ -4,7 +4,41 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+			      (Linux-specific) */
+};
 
 int main(int argc, char *argv[]){
+  if (argc < 2){
+    printf("no arguments");
+    return 1;
+  }
+  int key = ftok(argv[0], 22);
   
+  if (strcmp(argv[1], "-c") == 0){
+    int sd = shmget(key, 4, IPC_CREAT|0644);
+    struct shmid_ds buf;
+    int semid = semget(key, 1, IPC_CREAT|0644);
+    union semun data;
+    data.val = 1;
+    int file = open("story.txt", O_TRUNC, 0644);
+    shmctl(sd, IPC_SET, &buf);
+    semctl(semid, 0, IPC_SETVAL, data);
+    printf("sem val: %d\n", data);
+  }
+  else if (strcmp(argv[1], "-r") == 0){
+
+  }
+  else {
+    printf("invalid argument");
+  }
+  return 0;
 }
