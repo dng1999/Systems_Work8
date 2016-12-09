ee06b011550ab041bebb7d35f9e1ca00ebc66b5b
ThisIsMyNick <nobel.gautam@gmail.com>
2016-12-09 11:55:07 -0500

    add

--- rsctrl.c
+++ rsctrl.c
@@ -9,40 +9,54 @@
 #include <fcntl.h>
 
 union semun {
-  int              val;    /* Value for SETVAL */
-  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-  unsigned short  *array;  /* Array for GETALL, SETALL */
-  struct seminfo  *__buf;  /* Buffer for IPC_INFO
-			      (Linux-specific) */
+    int              val;    /* Value for SETVAL */
+    struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+    unsigned short  *array;  /* Array for GETALL, SETALL */
+    struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                                (Linux-specific) */
 };
 
 int main(int argc, char *argv[]){
-  if (argc < 2){
-    printf("no arguments");
-    return 1;
-  }
-  int key = ftok(argv[0], 22);
-  
-  if (strcmp(argv[1], "-c") == 0){
-    int shmid = shmget(key, 4, IPC_CREAT|0644);
-    struct shmid_ds buf;
-    int semid = semget(key, 1, IPC_CREAT|0644);
-    union semun data;
-    data.val = 1;
-    int file = open("story.txt", O_TRUNC, 0644);
+    if (argc < 2){
+        printf("no arguments");
+        return 1;
+    }
+    int key = ftok("Makefile", 22);
 
-    shmctl(shmid, IPC_STAT, &buf);
-    semctl(semid, 0, IPC_SETVAL, data);
-    printf("sem val: %d\n", data);
-  }
-  else if (strcmp(argv[1], "-r") == 0){
-    int shmid = shmget(key, 4, 0);
-    int semid = semget(key, 1, 0);
-    shmctl(shmid, IPC_RMID, &buf);
-    semctl(semid, 0, IPC_RMID);
-  }
-  else {
-    printf("invalid argument");
-  }
-  return 0;
+    if (strcmp(argv[1], "-c") == 0)
+    {
+        int shmid = shmget(key, 4, IPC_CREAT|0644);
+        int *len = shmat(shmid, NULL, 0);
+        *len = 0;
+
+        int semid = semget(key, 1, IPC_CREAT|0644);
+        union semun data;
+        data.val = 1;
+        semctl(semid, 0, SETVAL, data);
+    }
+    else if (strcmp(argv[1], "-r") == 0)
+    {
+        int shmid = shmget(key, 4, 0);
+        int *len = shmat(shmid, NULL, 0);
+        int semid = semget(key, 1, 0);
+
+        FILE *fp = fopen("story.txt", "r");
+        char s[512];
+        while (fgets(s, sizeof(s), fp))
+        {
+            printf("> %s\n", s);
+            memset(s, '0', sizeof(s));
+        }
+        shmctl(shmid, IPC_RMID, NULL);
+        semctl(semid, 0, IPC_RMID);
+    }
+    else
+    {
+        printf("Invalid argument.\n"
+                "Supported:\n"
+                "-c\t\tCreate\n"
+                "-r\t\tRemve\n"
+                "-v\t\tView\n");
+    }
+    return 0;
 }
--- writing.c
+++ writing.c
@@ -8,6 +8,41 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(){
+int main()
+{
+    int key = ftok("Makefile", 22);
+
+    int semid = semget(key, 1, 0);
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1;
+    if (semop(semid, &sb, 1) == -1)
+    {
+        printf("Resource unavailable.\n");
+        return 1;
+    }
+
+    int shmid = shmget(key, 4, 0);
+    int *len = shmat(shmid, NULL, 0);
+    if (*len > 0)
+    {
+        char *s = malloc(*len * sizeof(char));
+        int fd = open("story.txt", O_RDONLY);
+        lseek(fd, -*len, SEEK_END);
+        read(fd, s, *len * sizeof(char));
+        printf("%s\n", s);
+        free(s);
+    }
+    printf("Add: ");
+    char s[512];
+    fgets(s, sizeof(s), stdin);
+    int fd = open("story.txt", O_WRONLY);
+    write(fd, s, strlen(s));
+    close(fd);
+    *len = strlen(s);
+
+    sb.sem_op = 1;
+    semop(semid, &sb, 1);
 
 }
